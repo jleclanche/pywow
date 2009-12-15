32c044ce0dd0cf7fcb2dd1c96451a6cfa5f0f53f
Jerome Leclanche <jerome@leclan.ch>
2009-12-15 22:12:05 +0200

    wdbc: Added a logger class (patch by Андрей Рублев)

---
+++ logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Base logging for wdbc, all messages redirecting to stdout by default
+"""
+
+import sys
+import logging
+
+LOG_CONFIG = {
+	"stream": sys.stdout,
+	"format": "%(levelname)s: %(message)s"
+}
+
+class LazyLogger(object):
+	log = None
+	
+	@classmethod
+	def get_logger(cls):
+		if not cls.log:
+			logging.basicConfig(**LOG_CONFIG)
+			cls.log = logging.getLogger("wdbc")
+			cls.log.setLevel(logging.DEBUG)
+		return cls.log
+
+log = LazyLogger.get_logger()
