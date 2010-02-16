9b339482ba3b2896e563cf0f2b2a2e7205c32a5f
Jerome Leclanche <jerome@leclan.ch>
2010-02-16 16:04:41 +0200

    pywow: Add some more forgotten files

--- wdbc/structures/__init__.py
+++ wdbc/structures/__init__.py
@@ -1,1 +1,33 @@
 # -*- coding: utf-8 -*-
+
+from pywow.structures import Structure
+from .main import *
+from .custom import *
+from .generated import GeneratedStructure
+
+class StructureNotFound(Exception):
+	pass
+
+class StructureLoader():
+	wowfiles = None
+	
+	@classmethod
+	def setup(cls):
+		if cls.wowfiles is None:
+			cls.wowfiles = {}
+			for name in globals():
+				try:
+					if not issubclass(globals()[name], Structure):
+						continue
+				except TypeError:
+					continue
+				cls.wowfiles[name.lower()] = globals()[name]
+	
+	@classmethod
+	def getstructure(cls, name, build=0, parent=None):
+		if name in cls.wowfiles:
+			return cls.wowfiles[name](build, parent)
+		raise StructureNotFound("Structure not found for file %r" % (name))
+
+StructureLoader.setup()
+getstructure = StructureLoader.getstructure
---
+++ wdbc/structures/generated.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from pywow.structures import Skeleton, Structure
+from pywow.structures.fields import UnknownField
+
+
+class GeneratedStructure(Structure):
+	"""Dynamically generated DBC structure."""
+	def __init__(self, structure_string, *pargs, **kwargs):
+		columns = []
+		self.base = Skeleton(*[UnknownField() for s in structure_string])
+		Structure.__init__(self)
