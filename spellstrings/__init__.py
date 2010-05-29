df5601c313d2c8c25b01e98b17ba67496ebfacbd
Jerome Leclanche <jerome@leclan.ch>
2010-05-29 21:44:48 +0300

    spellstrings: Move the spellstrings parsing back to pywow.spellstrings rather than letting it live in sigrie. Modify it so we are able to use a proxy in order to get various values, instead of hardcoding field names and so on

---
+++ spellstrings/__init__.py
@@ -0,0 +1,647 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+from cStringIO import StringIO
+from datetime import timedelta
+from math import ceil, floor
+
+
+SEEK_CUR = 1 # os.SEEK_CUR
+
+BOOLEANS = ("g", "l")
+
+PAPERDOLL_VALUES = {
+	"ap":   "ATTACK_POWER",
+	"ar":   "ARMOR",
+	"bh":   "BONUS_HEALING",
+	"hnd":  "MAIN_WPN_HANDS",
+	"int":  "INTELLECT",
+	"mw":   "MAIN_WPN_DMG",
+	"mwb":  "MAIN_WPN_BASEDMG",
+	"mws":  "MAIN_WPN_SPEED",
+	"pa":   "PERCENT_ARCANE",
+	"pfi":  "PERCENT_FIRE",
+	"pfr":  "PERCENT_FROST",
+	"ph":   "PERCENT_HOLY",
+	"pn":   "PERCENT_NATURE",
+	"ps":   "PERCENT_SHADOW",
+	"pbh":  "PERCENT_BONUS_HEALING",
+	"pbhd": "PERCENT_BONUS_HEALING_DAMAGE",
+	"pl":   "PLAYER_LVL",
+	"rap":  "RANGED_ATTACK_POWER",
+	"rwb":  "RANGED_WPN_BASEDMG",
+	"sp":   "SPELL_POWER",
+	"spa":  "SPELL_POWER_ARCANE",
+	"spfi": "SPELL_POWER_FIRE",
+	"spfr": "SPELL_POWER_FROST",
+	"sph":  "SPELL_POWER_HOLY",
+	"spi":  "SPIRIT",
+	"spn":  "SPELL_POWER_NATURE",
+	"sps":  "SPELL_POWER_SHADOW",
+}
+
+FUNCTIONS = ["ceil", "cond", "eq", "floor", "gte", "gt", "max", "min"]
+
+class VariableNotFound(Exception):
+	"""
+	Raised when a description variable
+	does not exist.
+	"""
+	pass
+
+class Range(object):
+	def __init__(self, min, max):
+		self.min = abs(min)
+		self.max = abs(max)
+	
+	def __repr__(self):
+		return "%s(%r, %r)" % (self.__class__.__name__, self.min, self.max)
+	
+	def __str__(self):
+		if self.max > self.min:
+			return "%i to %i" % (self.min, self.max)
+		return str(int(self.max))
+	
+	def __cmp__(self, other):
+		return self.max - other
+	
+	def __mul__(self, value):
+		return Range(self.min * value, self.max * value)
+	
+	def __div__(self, value):
+		return Range(self.min / value, self.max / value)
+	
+	def __truediv__(self, value):
+		return self.__div__(value)
+
+
+class StringLookup(object):
+	"""
+	Lookup class for the parser
+	StringLookup will never do any parsing, but
+	only return proper formats for given values.
+	"""
+	def __init__(self, spell, proxy, braced=False):
+		self.spell = spell
+		self.last_value = 0
+		self.braced = braced # Whether or not we are inside braces
+		self.proxy = proxy
+	
+	def get_value(self, spell, value):
+		return self.proxy.get_value(self, spell, value)
+	
+	def get_effect(self, spell, effect, ordering):
+		return self.proxy.get_effect(self, spell, effect, ordering)
+	
+	def get_spell(self, id):
+		return self.proxy.get_spell(self, id)
+	
+	def __macro_M(self, spell, identifier, effect):
+		value = self.get_effect(spell, "damage_base", effect)
+		sides = self.get_effect(spell, "die_sides", effect)
+		dice = 1
+		return value + (sides * dice)
+	
+	def __macro_m(self, spell, identifier, effect):
+		value = self.get_effect(spell, "damage_base", effect)
+		sides = self.get_effect(spell, "die_sides", effect)
+		return value + min(sides, 1)
+	
+	def __macro_s(self, spell, identifier, effect):
+		min = self.__macro_m(spell, identifier, effect)
+		max = self.__macro_M(spell, identifier, effect)
+		return Range(min, max)
+	
+	def __macro_t(self, spell, identifier, effect):
+		return self.get_effect(spell, "aura_interval", effect) / 1000
+	
+	
+	def format_boolean(self, identifier, values):
+		if identifier == "g":
+			return "<%s/%s>" % (values)
+		
+		if identifier == "l":
+			x = self.last_value
+			if self.last_value > 1:
+				return values[1]
+			return values[0]
+	
+	def format_duration(self, duration):
+		# TODO make this prettier, remove the braced conds
+		if not isinstance(duration, timedelta):
+			duration = timedelta(microseconds=duration)
+		
+		if duration <= timedelta(milliseconds=0):
+			if self.braced:
+				return "0"
+			return "until cancelled"
+		
+		if duration < timedelta(minutes=1):
+			if self.braced:
+				return str(duration.seconds)
+			return "%d sec" % (duration.seconds)
+		
+		if duration < timedelta(hours=1):
+			if self.braced:
+				return str(duration.seconds / 60)
+			return "%d min" % (duration.seconds / 60)
+		
+		if duration == timedelta(hours=1):
+			if self.braced:
+				return str(duration.seconds / 3600)
+			return "%d hour" % (duration.seconds / 3600)
+		
+		if duration < timedelta(days=1):
+			if self.braced:
+				return str(duration.seconds / 3600)
+			return "%d hrs" % (duration.seconds / 3600)
+		
+		if self.braced:
+			return str(duration.days)
+		return "%d days" % (duration.days)
+	
+	def format_function(self, identifier, args):
+		return "%s(%s)" % (identifier, args)
+	
+	def format_macro(self, spell, identifier, effect):
+		spell = self.get_spell(spell)
+		effect = int(effect)
+		if not spell:
+			return "$" + identifier + str(effect)
+		
+		if identifier == "A":
+			value = self.get_effect(spell, "radius_max", effect)
+			if not value: # FIXME is this right?
+				value = self.get_effect(spell, "radius_min", effect)
+			return int(value)
+		
+		if identifier == "a":
+			value = self.get_effect(spell, "radius_min", effect)
+			return int(value)
+		
+		if identifier == "bc": # 48165, 69406, 69416
+			return self.get_effect(spell, "multiplier", effect)
+		
+		if identifier == "b":
+			return self.get_effect(spell, "points_combo", effect)
+		
+		if identifier == "d":
+			value = self.get_value(spell, "duration_1") # FIXME use duration class
+			return self.format_duration(value)
+		
+		if identifier == "e":
+			return self.get_effect(spell, "amplitude", effect)
+		
+		if identifier == "f" or identifier == "F":
+			value = self.get_effect(spell, "chain_amplitude", effect)
+			return value
+		
+		if identifier == "h":
+			return self.get_value(spell, "proc_chance")
+		
+		if identifier == "i":
+			return self.get_value(spell, "max_targets")
+		
+		if identifier == "M":
+			return self.__macro_M(spell, identifier, effect)
+		
+		if identifier == "m":
+			return self.__macro_m(spell, identifier, effect)
+		
+		if identifier == "n":
+			return self.get_value(spell, "proc_charges")
+		
+		if identifier == "o":
+			duration = self.get_value(spell, "duration_1") / 1000
+			tick = self.get_effect(spell, "aura_interval", effect) or 5000
+			damage = self.__macro_s(spell, identifier, effect)
+			return damage * int(duration / tick)
+		
+		if identifier == "q":
+			return self.get_effect(spell, "misc_value_1", effect)
+		
+		if identifier == "r":
+			return self.get_value(spell, "range_max")
+		
+		if identifier == "s" or identifier == "S":
+			return self.__macro_s(spell, identifier, effect)
+		
+		if identifier == "t":
+			value = self.__macro_t(spell, identifier, effect)
+			if value != int(value): # FIXME: WTF?
+				return "%.2f" % (value)
+			return int(value)
+		
+		if identifier == "u":
+			return self.get_value(spell, "stack")
+		
+		if identifier == "v":
+			return self.get_value(spell, "max_target_level")
+		
+		if identifier == "x":
+			return self.get_effect(spell, "chain_targets", effect)
+		
+		if identifier == "z":
+			return "<Home>"
+	
+	
+	def format_paperdoll(self, identifier):
+		return identifier
+
+
+class SpellString(str):
+	"""
+	Parsing class
+	We pass all sorts of formatting and
+	lookups to the StringLookup class
+	"""
+	def __read_alpha(self, buffer):
+		"""
+		Parse a chain of alphabetic characters
+		"""
+		ret = []
+		token = buffer.read(1)
+		while token.isalpha():
+			ret.append(token)
+			token = buffer.read(1)
+		if token:
+			buffer.seek(-1, SEEK_CUR)
+		return "".join(ret)
+	
+	def __read_alphanum(self, buffer):
+		ret = []
+		token = buffer.read(1)
+		
+		while token.isalnum():
+			ret.append(token)
+			token = buffer.read(1)
+		
+		buffer.seek(-1, SEEK_CUR)
+		return "".join(ret)
+	
+	def __read_block(self, buffer, startchr, endchr):
+		"""
+		Parse a string from startchr until endchr
+		"""
+		ret = []
+		count = 0
+		token = buffer.read(1)
+		while token != startchr:
+			token = buffer.read(1)
+			if not token:
+				raise ValueError("read_block could not find beginning of block")
+		count += 1
+		
+		while count:
+			token = buffer.read(1)
+			if token == startchr:
+				count += 1
+			elif token == endchr:
+				count -= 1
+			if count:
+				ret.append(token)
+			if not token:
+				break
+		
+		return "".join(ret)
+	
+	def __read_number(self, buffer):
+		"""
+		Parse a chain of numeric characters
+		and build a number with them
+		"""
+		ret = []
+		token = buffer.read(1)
+		while token.isdigit():
+			ret.append(token)
+			token = buffer.read(1)
+		buffer.seek(-1, SEEK_CUR)
+		return int("".join(ret) or 0)
+	
+	def __read_until(self, buffer, char, break_space=False):
+		"""
+		Helper to parse until a specific character
+		"""
+		ret = []
+		token = buffer.read(1)
+		while token != char:
+			if break_space and token.isspace():
+				return
+			ret.append(token)
+			token = buffer.read(1)
+			if not token:
+				break
+		return "".join(ret)
+	
+	
+	def __parse_boolean(self, buffer):
+		"""
+		Parse the immediately available
+		boolean from the buffer and
+		return both values.
+		Example:
+		Summon $s $lan ally:many allies;...
+		The $gsorcerer:sorceress; casts...
+		"""
+		val1 = self.__read_until(buffer, ":")
+		val2 = self.__read_until(buffer, ";")
+		
+		return val1, val2
+	
+	
+	def __parse_conditional_condition(self, buffer):
+		"""
+		Parse the immediately available conditional condition
+		Example:
+		(s56810 |s25306|!((!a48165)|a66109))
+		"""
+		token = buffer.read(1)
+		if token == "$":
+			identifier = self.__parse_next(buffer)
+		else:
+			buffer.seek(-1, SEEK_CUR)
+			if token == "(":
+				xbuffer = self.__read_block(buffer, startchr="(", endchr=")")
+				xbuffer = StringIO(xbuffer)
+				identifier = self.__read_alphanum(xbuffer)
+			else:
+				identifier = self.__read_alphanum(buffer)
+		return identifier
+	
+	def __parse_conditional(self, buffer):
+		"""
+		Parse the immediately available conditional block
+		"""
+		identifier = self.__parse_conditional_condition(buffer)
+		
+		value_if = self.__read_block(buffer, startchr="[", endchr="]")
+		value_if = SpellString(value_if)
+		value_if = value_if.format(self.obj, proxy=self.proxy)
+		value_else = self.__read_block(buffer, startchr="[", endchr="]")
+		value_else = SpellString(value_else).format(self.obj, proxy=self.proxy)
+		return identifier, value_if, value_else
+	
+	
+	def __parse_function_args(self, buffer):
+		"""
+		Parse the immediately available
+		block of function arguments,
+		returning a tuple of the arguments.
+		"""
+		
+	
+	def __parse_macro(self, buffer):
+		"""
+		Parse the immediately available
+		macro or variable and return
+		the spell, identifier and effect
+		"""
+		
+		# Parse an optional id
+		spell = self.__read_number(buffer)
+		
+		##
+		# FIXME technically, the effects do not exist, as
+		# WoW parses the identifier alphanumerically
+		# Do we really want that? It's easier to go back.
+		
+		# Parse an alphabetic identifier
+		identifier = self.__read_alpha(buffer)
+		
+		# effect id should be the next char
+		effect = buffer.read(1)
+		
+		# However it's optional. If it's not here
+		# we have to go back one char.
+		if effect and effect not in ("1", "2", "3"):
+			buffer.seek(-1, SEEK_CUR)
+			effect = ""
+		
+		return spell, identifier, effect
+	
+	
+	def __parse_operator(self, buffer):
+		num = self.__read_until(buffer, ";", break_space=True)
+		try:
+			num = int(num)
+		except TypeError:
+			return None, None
+		
+		spell, identifier, effect = self.__parse_macro(buffer)
+		var = self.formatter.format_macro(spell, identifier, effect)
+		
+		return num, var
+	
+	
+	def __parse_next(self, buffer):
+		"""
+		Parse the immediately available
+		variable from the buffer and
+		return the formatted result.
+		"""
+		token = buffer.read(1)
+		
+		_tell = buffer.tell()
+		# Is it an operator?
+		if token == "/":
+			num, var = self.__parse_operator(buffer)
+			if num is None:
+				buffer.seek(_tell - 1)
+				return "$"
+			
+			if isinstance(var, str):
+				return var
+			
+			ret = (var / num)
+			if isinstance(ret, Range):
+				ret = ret.min # XXX is this right?
+			if int(ret) != ret:
+				return "%.1f" % ret
+			return str(int(ret))
+		
+		if token == "*":
+			num, var = self.__parse_operator(buffer)
+			ret = var * num
+			if isinstance(ret, float):
+				ret = int(round(ret))
+			return str(ret)
+		
+		# Is it a conditional?
+		if token == "?":
+			identifier, value1, value2 = self.__parse_conditional(buffer)
+			return value2
+		
+		if token == "<":
+			buffer.seek(-1, SEEK_CUR)
+			identifier = self.__read_block(buffer, startchr="<", endchr=">")
+			try:
+				value = self.get_variable(identifier)
+				return SpellString(value).format(self.obj, proxy=self.proxy)
+			except VariableNotFound:
+				return "<%s>" % (identifier)
+		
+		if token == "{":
+			buffer.seek(-1, SEEK_CUR)
+			block = self.__read_block(buffer, startchr="{", endchr="}")
+			
+			# Attempt to read decimals formatting
+			decimals = 0
+			token = buffer.read(1)
+			if token == ".":
+				decimals = self.__read_number(buffer)
+			elif token:
+				# Step one char back, only if we are not at the end
+				buffer.seek(-1, SEEK_CUR)
+			
+			block = SpellString(block).format(self.obj, proxy=self.proxy, braced=True)
+			try: # FIXME
+				block = eval(block)
+				if isinstance(block, float):
+					formatting = "%%.%if" % decimals
+				else:
+					formatting = "%i"
+				return formatting % (block)
+			except Exception:
+				return "[%s]" % (block)
+		
+		# At this point, we need to check for functions and variables
+		# but only if we don't already have a digit
+		if not token.isdigit():
+			_tell = buffer.tell()
+			buffer.seek(-1, SEEK_CUR)
+			identifier = self.__read_alpha(buffer)
+			
+			if identifier in FUNCTIONS:
+				args = self.__parse_function_args(buffer)
+				return self.formatter.format_function(identifier, args)
+			
+			if identifier.lower() in PAPERDOLL_VALUES:
+				return self.formatter.format_paperdoll(identifier)
+			
+			
+			# We didn't find any valid identifier
+			if not identifier:
+				return "$"
+			
+			# Nothing left to check for but booleans
+			# The values get messed with the identifier however, so we need to
+			# look at only the first char
+			if identifier[0] in BOOLEANS:
+				identifier = identifier[0]
+				buffer.seek(_tell)
+				values = self.__parse_boolean(buffer)
+				return self.formatter.format_boolean(token, values)
+		
+		# It's probably a variable then
+		buffer.seek(-1, SEEK_CUR)
+		spell, identifier, effect = self.__parse_macro(buffer)
+		
+		if identifier:
+			spell = int(spell or 0)
+			effect = int(effect or 1)
+			
+			value = self.formatter.format_macro(spell, identifier, effect)
+			self.formatter.last_value = value
+			return str(value)
+		
+		
+		if not token or token.isspace():
+			return token
+		
+		return token
+	
+	def get_variable(self, identifier):
+		"""
+		Queries the cache for a description variable
+		or builds the cache if there isn't one yet.
+		"""
+		if not hasattr(self, "__sdvcache"):
+			self.__sdvcache = {}
+			buffer = str(self.obj.description_variables.variables)
+			buffer = StringIO(buffer)
+			token = buffer.read(1)
+			while token:
+				if token == "$":
+					variable = self.__read_until(buffer, "=")
+					value = self.__read_until(buffer, "\n").strip()
+					self.__sdvcache[variable] = value
+				token = buffer.read(1)
+		
+		if identifier not in self.__sdvcache:
+			raise VariableNotFound(identifier)
+		
+		return self.__sdvcache[identifier]
+	
+	def format(self, obj, proxy, braced=False):
+		if "$" not in self:
+			return self
+		self.obj = obj
+		self.proxy = proxy
+		self.formatter = StringLookup(obj, proxy=proxy, braced=braced)
+		buffer = StringIO(self)
+		ret = []
+		
+		while True:
+			token = buffer.read(1)
+			if not token: # string is over
+				break
+			
+			if token == "$":
+				ret.append(self.__parse_next(buffer))
+			else:
+				ret.append(token)
+		
+		return "".join(ret)
+
+
+class WDBCProxy(object):
+	"""
+	Default proxy for wdbc-like structure
+	"""
+	
+	value_lookup = {
+		"radius_min": "radius__radius_min"
+	}
+	
+	@classmethod
+	def get_value(self, instance, spell, value):
+		if value == "duration_1":
+			duration = spell.duration
+			if not duration:
+				return 0
+			return duration._raw("duration_1") * 1000
+		
+		if value in self.value_lookup:
+			value = self.value_lookup[value]
+		
+		try:
+			return getattr(spell, value)
+		except AttributeError, e:
+			return 0
+	
+	effect_lookup = {
+		"radius_min_effect_%i": "radius_effect_%i__radius_min",
+	}
+	
+	@classmethod
+	def get_effect(self, instance, spell, effect, ordering):
+		field = "%s_effect_%%i" % (effect)
+		if field in self.effect_lookup:
+			field = self.effect_lookup[field]
+		field = field % (ordering)
+		try:
+			return getattr(spell, field)
+		except AttributeError, e:
+			return 0
+	
+	@classmethod
+	def get_spell(self, instance, id):
+		spell_id = instance.spell._id
+		id = id or spell_id
+		if id == spell_id:
+			return instance.spell
+		try:
+			return instance.spell._parent[id]
+		except KeyError:
+			return None
---
+++ spellstrings/tests.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+TESTS = { # 11927
+	 1010: "Curse the target with idiocy, reducing Intellect and Spirit by 6 every 3 seconds until each is reduced by a total of 90.  Only one Curse per Warlock can be active on any one target.",
+	 2687: "Generates 20 rage at the cost of health, and then generates an additional 10 rage over 10 sec.",
+	 2792: "Nullifies corrupting effects on the friendly target, removing 1 magical effect, 1 curse, and $ poison.",
+	 8024: "Imbue the Shaman's weapon with fire, increasing total spell damage by 7. Each hit causes 4.2 to 13.0 additional Fire damage, based on the speed of the weapon.  Slower weapons cause more fire damage per swing.  Lasts 30 minutes.",
+	11213: "Gives you a 2% chance of entering a Clearcasting state after any damage spell hits a target.  The Clearcasting state reduces the mana cost of your next damage spell by 100%.",
+	13797: "Place a fire trap that will burn the first enemy to approach for $<damage> Fire damage over 15 sec.  Trap will exist for 30 sec.  Only one trap can be active at a time.",
+	14253: "Attempts to cure 1 poison effect on the target, and 1 more poison effect every 2 seconds for 8 sec.",
+	16190: "Summons a Mana Tide Totem with 10% of the caster's health at the feet of the caster for 12 sec that restores 6% of total mana every 3 seconds to group members within 30 yards.",
+	21228: "Increases the duration of your Arcane Missiles by 1 sec.",
+	23025: "Reduces the cooldown of your Blink spell by 2 sec.",
+	29907: "Hits an enemy with an anti-mana bolt that consumes up to 255 to 295 mana. For each point of mana consumed by the bolt, the target takes 1.0 damage.",
+	29977: "When activated, this spell causes each of your Fire damage spell hits to increase your critical strike chance with Fire damage spells by 10%.  This effect lasts until you have caused 3 critical strikes with Fire spells.",
+	31687: "Summon a Water Elemental to fight for the caster for 45 sec.",
+	33127: "Gives the Paladin a chance to deal additional Holy damage equal to 36% of normal weapon damage.  Only one Seal can be active on the Paladin at any one time.  Lasts 30 sec.\r\n\r\nUnleashing this Seal's energy will judge an enemy, instantly causing $s1 Holy damage, $s1 if the target is stunned or incapacitated.",
+	36459: "Summon a Water Elemental to fight for the caster for until cancelled.",
+	36950: "Causes up to 5 nearby enemies to flee for 6 sec.",
+	37360: "Imbue your weapon with power, increasing attack power against undead and demons by 150.  Lasts 5 min.  Cannot be used on items level 138 and higher.",
+	39758: "Soothes the target, reducing the range at which it will attack you by 1000 yards.  Only affects Humanoid targets level 200 or lower.  Lasts 1 hour.",
+	39794: "Brings a dead player back to life with 1 health and 0 mana.  Cannot be cast when in combat.",
+	47471: "Attempt to finish off a wounded foe, causing [1456+AP*0.2] damage and converting each extra point of rage into 38 additional damage (up to a maximum cost of 30 rage).  Only usable on enemies that have less than 20% health.",
+	48677: "Throw burning oil at your pursuers slowing them down and inflicting 21336 to 42664 Fire damage over 8 sec.",
+	49628: "Your auto attacks have a 30% chance to cause a Blood-Caked Strike, which hits for 25% weapon damage plus 12.5% for each of your diseases on the target.",
+	53717: "Cause a corpse to explode for 60 Nature damage modified by attack power to all enemies within 20 yards.  Will use a nearby corpse if none is targeted.  Does not affect mechanical or elemental corpses.",
+	57861: "Lets loose a roar, Increaseing damage taken by 20% for $.",
+	58425: "Your finishing moves have a 20% chance per combo point to restore 25 energy.",
+	58644: "Reduces the cost of your Frost Strike by -5864.",
+	58645: "Restores 18480 health and 12840 mana over 30 sec.  Must remain seated while eating.",
+	61905: "Summons a Magma Totem with 5000 health at the feet of the caster for -1 sec that causes 371 Fire damage to creatures within 8 yards every 2 seconds.",
+	64936: "Shield Block also grants you $s1% reduction to magical damage taken.",
+	69561: "Restores 19200 mana over 30 sec.  Must remain seated while drinking.  If you spend at least 10 seconds drinking you will become \"well fed\" and gain 40 critical strike rating for 1 hour. Standard alcohol.",
+	71180: "Reduces the cast time of your next Frostbolt or Frostfire Bolt by $/1000S1 sec.  Lasts 10 sec.",
+	72930: "Chills a random target, reducing movement speed by 50% and dealing significant Frost damage after 14 sec.",
+	75878: "Deals 75000 to 85000 Fire damage to enemies within 0 yards and spawns a Living Inferno.",
+	76332: "The Corrupted Blaze inflicts 3900 to 4100 Fire damage every 1 sec and increases Fire damage taken by 1% for 5 sec.",
+	78273: "Imbue the Shaman's weapon with fire, increasing total spell damage by 7. Each hit causes $/78275m1 to 1 additional Fire damage. Lasts 30 minutes.",
+	78972: "Siphon the energies out of the Twilight Devotees and Faceless Monitors in a 10 yard radius around the Possessed Ascendant. This deals 72000 to 88000 damage to them, while healing the Possessed Ascendant for 72000 to 88000 health and increasing its damage and size by 25% for 15 sec sec."
+}
+
+def main():
+	from pywow.wdbc.environment import Environment
+	from pywow.spellstrings import SpellString
+	
+	f = Environment(11723)["Spell.dbc"]
+	
+	fail, success = 0, 0
+	for id, expected in TESTS.items():
+		print "Testing %i..." % (id),
+		if id not in f:
+			print "PASS"
+			continue
+		
+		spell = f[id]
+		description = SpellString(spell.description_enus).format(spell, proxy=WDBCProxy)
+		
+		if description != expected:
+			print "FAIL"
+			print "Returned:", repr(description)
+			print "Expected:", repr(expected)
+			fail += 1
+		else:
+			print "OK"
+			success += 1
+	
+	print "%i/%i tests passed, %i failure" % (success, fail+success, fail)
+
+if __name__ == "__main__":
+	main()
