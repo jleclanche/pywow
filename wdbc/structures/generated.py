bec238045a63c50f879c1128543bfb445b6141ba
Jerome Leclanche <jerome@leclan.ch>
2010-06-18 03:54:08 +0300

    structures/generated: Always have an IDField as first column in Generated Structures

--- wdbc/structures/generated.py
+++ wdbc/structures/generated.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from ..structures import Skeleton, Structure, UnknownField
+from ..structures import Skeleton, Structure, UnknownField, IDField
 
 
 class GeneratedStructure(Structure):
 	"""Dynamically generated DBC structure."""
 	def __init__(self, structure_string, *pargs, **kwargs):
 		columns = []
-		self.fields = Skeleton(*[UnknownField() for s in structure_string])
+		fields = [IDField()] + [UnknownField() for s in structure_string][1:]
+		self.fields = Skeleton(*fields)
 		Structure.__init__(self)
