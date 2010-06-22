94b16f85475e5498567de596c89dae3fc64afd0b
Jerome Leclanche <jerome@leclan.ch>
2010-06-22 03:45:06 +0300

    spellstrings/tests: Add a few tests

--- spellstrings/tests.py
+++ spellstrings/tests.py
@@ -8,6 +8,7 @@ TESTS[11723] = {
 	 1010: "Curse the target with idiocy, reducing Intellect and Spirit by 6 every 3 seconds until each is reduced by a total of 90.  Only one Curse per Warlock can be active on any one target.",
 	 2687: "Generates 20 rage at the cost of health, and then generates an additional 10 rage over 10 sec.",
 	 8024: "Imbue the Shaman's weapon with fire, increasing total spell damage by 7. Each hit causes 4.2 to 13.0 additional Fire damage, based on the speed of the weapon.  Slower weapons cause more fire damage per swing.  Lasts 30 minutes.",
+	10390: "Causes an explosion of arcane magic around the caster, causing 0 damage to all targets within 0 yards.",
 	11213: "Gives you a 2% chance of entering a Clearcasting state after any damage spell hits a target.  The Clearcasting state reduces the mana cost of your next damage spell by 100%.",
 	14253: "Attempts to cure 1 poison effect on the target, and 1 more poison effect every 2 seconds for 8 sec.",
 	16190: "Summons a Mana Tide Totem with 10% of the caster's health at the feet of the caster for 12 sec that restores 6% of total mana every 3 seconds to group members within 30 yards.",
@@ -41,8 +42,10 @@ TESTS[11723] = {
 _ = TESTS[12266] = TESTS[11723].copy()
 del _[1010], _[37360], _[47471], _[57861], _[75878]
 _[14253] = "Attempts to cure 1 poison effect on the target, and $s1 more poison effect every 2 seconds for 8 sec."
+_[15349] = "Increases your critical healing effect chance by 2% for each harmful damage-over-time effect on your target up to 2 effects or -20% increased critical healing effect chance."
 _[16190] = "Summons a Mana Tide Totem with 10% of the caster's health at the feet of the caster for 12 sec that restores 6% of total mana every 3 seconds to group members within 10 yards." # scaling
 _[61905] = "Summons a Magma Totem with 5000 health at the feet of the caster for -1 sec that causes $s1 Fire damage to creatures within $a1 yards every $t1 seconds."
+_[78273] = "Imbue the Shaman's weapon with fire, increasing total spell damage by 7. Each hit causes $/78275m1 to 1 additional Fire damage. Lasts 30 minutes."
 
 def main():
 	from pywow.wdbc.environment import Environment
