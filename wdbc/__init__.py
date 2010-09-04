11537c345e45792602cc602f998cb3d865b5963f
Jerome Leclanche <jerome@leclan.ch>
2010-09-04 10:14:42 +0100

    wdbc: Move DBC, WCF file logic to wdbc.dbc

--- wdbc/__init__.py
+++ wdbc/__init__.py
@@ -6,7 +6,6 @@ from struct import pack, unpack, error as StructError
 from .log import log
 from .structures import fields, StructureNotFound, getstructure
 from .utils import getfilename, generate_structure
-from .wdb import WDBFile
 
 ##
 # Header classes
@@ -18,18 +17,6 @@ class DBHeader(object):
 	"""
 	def __repr__(self):
 		return "%s(%s)" % (self.__class__.__name__, ", ".join("%s=%r" % (k, self.__dict__[k]) for k in self.__dict__))
-
-class DBCHeader(DBHeader):
-	def __len__(self):
-		return 20
-	
-	def load(self, file):
-		file.seek(0)
-		data = file.read(len(self))
-		self.signature, self.row_count, self.field_count, self.reclen, self.stringblocksize = unpack("<4s4i", data)
-	
-	def data(self):
-		return pack("<4s4i", self.signature, self.row_count, self.field_count, self.reclen, self.stringblocksize)
 
 class DB2Header(DBHeader):
 	def __len__(self):
@@ -189,167 +176,6 @@ class DBFile(object):
 			self.file = open(f.name, "rb")
 
 
-class DBCFile(DBFile):
-	"""
-	A DBC file.
-	- Each file has an arbitrary amount of columns always of the same length and
-	  structure (defined in the header).
-	- Each string is a 32-bit pointer to an address inside the stringblock, starting
-	  from 0 as the stringblock address (defined in the header as well).
-	- EOF is 1 NULL byte, followed by the stringblock if there is one.
-	- The stringblock is a non-repetitive block of null-terminated strings.
-	"""
-	
-	def __init__(self, file, build, structure, environment):
-		super(DBCFile, self).__init__(file, build, structure, environment)
-		self.header = DBCHeader()
-		self.header.load(file)
-		if not build:
-			build = 0
-		self.build = build
-		self.__load_structure(structure)
-	
-	def __check_padding(self, file, field):
-		"""
-		In 4.0.0 DBCs, fields are padded to their own size
-		within the file. Example:
-		byte, int -> byte, pad, pad, pad, int
-		"""
-		address = file.tell()
-		seek = (address % field.size)
-		seek = seek and -(seek - field.size)
-		file.seek(seek, os.SEEK_CUR)
-	
-	def __load_structure(self, structure):
-		name = getfilename(self.file.name)
-		try:
-			self.structure = getstructure(name, self.build, parent=self)
-		except StructureNotFound:
-			self.structure = generate_structure(self)
-		
-		# Generate the Localized Fields
-		fieldidx = []
-		for i, field in enumerate(self.structure):
-			if isinstance(field, structures.LocalizedField):
-				fieldidx.append((i, field.name))
-		
-		if fieldidx:
-			from copy import copy
-			fields = structures.LocalizedStringField(build=self.build)
-			for i, name in reversed(fieldidx):
-				# Build a copy of the fields
-				toinsert = [copy(field).rename("%s_%s" % (name, field.name)) for field in fields]
-				self.structure[i:i+1] = toinsert
-		
-		log.info("Using %s build %i" % (self.structure, self.build))
-		
-		self.check_integrity()
-	
-	def _parse_field(self, data, field, row=None):
-		if self.build in (11927, 12025): # TODO pywow.builddata
-			self.__check_padding(data, field)
-		return super(DBCFile, self)._parse_field(data, field, row)
-	
-	def _parse_row(self, id):
-		address, reclen = self._addresses[id]
-		self.file.seek(address)
-		data = self.file.read(reclen) # We also read id and reclen columns
-		row = DBRow(self, data=data)
-		self._values[id] = row
-	
-	def _parse_string(self, data):
-		address, = unpack("<I", data.read(4))
-		if not address:
-			return ""
-		
-		f = self.file
-		pos = f.tell()
-		f.seek(-self.header.stringblocksize, os.SEEK_END) # Go to the stringblock
-		f.seek(address, os.SEEK_CUR) # seek to the address in the stringblock
-		
-		# Read until \0
-		chars = []
-		while True:
-			char = f.read(1)
-			if char == "\0":
-				break
-			if not char:
-				if not chars:
-					#log.warning("No string found at 0x%08x (%i), some values may be corrupt. Fix your structures!" % (address, address - self.header.stringblocksize))
-					return ""
-				log.warning("Unfinished string, this file may be corrupted.")
-				break
-			chars.append(char)
-		
-		f.seek(pos)
-		
-		return "".join(chars)
-	
-	def check_integrity(self):
-		reclen = self.header.reclen
-		struct_len = self.structure._reclen()
-		if struct_len != reclen:
-			log.warning("File structure does not respect DBC reclen. Expected %i, reading %i. (%+i)" % (reclen, struct_len, reclen-struct_len))
-		
-		field_count = self.header.field_count
-		total_fields = len(self.structure)
-		if field_count != total_fields:
-			log.warning("File structure does not respect DBC field count. Expected %i, got %i instead." % (field_count, total_fields))
-	
-	def data(self):
-		ret = []
-		self.__stringblock = []
-		address_lookup = {}
-		address = 1
-		for row in self:
-			row = self[row]
-			row._save()
-			_data = []
-			for field, value in zip(self.structure, row):
-				if isinstance(field, fields.StringField):
-					if not value:
-						_value = 0
-					elif value in address_lookup: # the string is already in the stringblock
-						_value = address_lookup[value]
-					else:
-						_value = address
-						address_lookup[value] = address
-						self.__stringblock.append(value)
-						address += len(value) + 1
-					value = pack("<I", _value)
-				
-				else:
-					value = pack("<%s" % (field.char), value)
-				
-				_data.append(value)
-			ret.append("".join(_data))
-		return "".join(ret)
-	
-	def eof(self):
-		return "\0" + ("\0".join(self.__stringblock)) + "\0"
-	
-	def preload(self):
-		f = self.file
-		f.seek(len(self.header))
-		if isinstance(self, DB2File):
-			print f.seek(self.header.get_block_size())
-		
-		rows = 0
-		field = self.structure[0]
-		row_header_size = field.size
-		reclen = self.header.reclen
-		while rows < self.header.row_count:
-			address = f.tell() # Get the address of the full row
-			id = self._parse_field(f, field)
-			
-			self._add_row(id, address, reclen)
-			
-			f.seek(reclen - row_header_size, os.SEEK_CUR) # minus length of id
-			rows += 1
-		
-		log.info("%i rows total" % (rows))
-
-
 class DB2File(DBCFile):
 	"""
 	New DB format introduced in build 12803
@@ -385,32 +211,6 @@ class DB2File(DBCFile):
 		
 		log.info("Using %s build %i" % (self.structure, self.build))
 		self.check_integrity()
-
-
-class WCFFile(DBCFile):
-	"""
-	Pretty much a DBC file without a header.
-	Currently only used with baddons.wcf.
-	"""
-	def preload(self):
-		f = self.file
-		f.seek(0)
-		rows = 0
-		field = self.structure[0]
-		reclen = sum(k.size for k in self.structure)
-		row_header_size = field.size
-		while True:
-			address = f.tell() # Get the address of the full row
-			id = self._parse_field(f, field)
-			if id is None:
-				break
-			
-			self._add_row(id, address, reclen)
-			
-			f.seek(reclen - row_header_size, os.SEEK_CUR) # minus length of id
-			rows += 1
-		
-		log.info("%i rows total" % (rows))
 
 
 class DBRow(list):
@@ -591,65 +391,6 @@ class DBRow(list):
 	def id(self):
 		"Temporary hack to transition between _id and id"
 		return self._id
-
-
-class InferredDBCFile(DBCFile):
-	"""
-	DBCFile with implicit ordering. These files have no IDField.
-	"""
-	
-	def preload(self):
-		f = self.file
-		f.seek(len(self.header))
-		
-		rows = 0
-		reclen = self.header.reclen
-		while rows < self.header.row_count:
-			address = f.tell() # Get the address of the full row
-			id = rows + 1
-			
-			self._addresses[id] = (address, reclen)
-			
-			f.seek(reclen, os.SEEK_CUR)
-			rows += 1
-		
-		log.info("%i rows total" % (rows))
-
-
-class ComplexDBCFile(DBCFile):
-	"""
-	Only used in ItemSubClass.dbc for now
-	Two IDFields.
-	"""
-	
-	def preload(self):
-		f = self.file
-		f.seek(len(self.header))
-		
-		rows = 0
-		reclen = self.header.reclen
-		while rows < self.header.row_count:
-			address = f.tell() # Get the address of the full row
-			id = unpack("<ii", f.read(8)) # id is a tuple instead
-			
-			if id in self._addresses: # Something's wrong here
-				log.warning("Multiple instances of row %s found" % (".".join(id)))
-			self._addresses[id] = (address, reclen)
-			
-			f.seek(reclen - 8, os.SEEK_CUR) # minus 4 bytes for each id
-			rows += 1
-		
-		log.info("%i rows total" % (rows))
-
-
-class UnknownDBCFile(DBCFile):
-	"""
-	A DBC file with an unknown structure.
-	"""
-	writable = False
-	def load_structure(self, filename=None, build=None):
-		self.structure = self._generate_structure()
-		log.info("Using generated structure for file %s, build %i" % (self.filename, self.build))
 
 
 def fopen(name, build=0, structure=None, environment={}):
