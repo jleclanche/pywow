4389fa950c72b400678395d35a5b484fa4b6940e
Jerome Leclanche <jerome@leclan.ch>
2010-09-06 07:02:21 +0100

    wdbc: Work around a .adb bug which causes strings to be off by one

--- wdbc/db2.py
+++ wdbc/db2.py
@@ -20,6 +20,10 @@ class DB2Header(DBHeader):
 		file.seek(0)
 		self.signature, self.row_count, self.field_count, self.reclen, self.stringblocksize, self.dbhash, self.build, self.timestamp = unpack("<4s7i", file.read(32))
 		if self.build >= 12834:
+			if self.signature == "WCH2":
+				# Work around a bug in cataclysm beta which doesn't take in account the first \0
+				log.warning("Old adb file, working around stringblock bug")
+				self.stringblocksize += 1
 			self.unk1, self.unk2, self.locale, self.unk3 = unpack("<4i", file.read(16))
 	
 	def get_block_size(self):
