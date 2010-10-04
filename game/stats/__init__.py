ca4ee5b4244c90dde8d9ae71c685fda3a0043979
Jerome Leclanche <jerome@leclan.ch>
2010-10-04 16:16:54 +0100

    game/stats: Add stats module

---
+++ game/stats/__init__.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+from .. import globalstrings
+
+## Possibly useful
+#ITEM_MOD_DAMAGE_PER_SECOND_SHORT = "Damage Per Second"
+#ITEM_MOD_MASTERY_RATING_SPELL = "(%s)"
+#ITEM_MOD_MASTERY_RATING_TWO_SPELLS = "(%s/%s)"
+#ITEM_MOD_POWER_REGEN0_SHORT = "Mana Per 5 Sec."
+#ITEM_MOD_POWER_REGEN1_SHORT = "Rage Per 5 Sec."
+#ITEM_MOD_POWER_REGEN2_SHORT = "Focus Per 5 Sec."
+#ITEM_MOD_POWER_REGEN3_SHORT = "Energy Per 5 Sec."
+#ITEM_MOD_POWER_REGEN4_SHORT = "Happiness Per 5 Sec."
+#ITEM_MOD_POWER_REGEN5_SHORT = "Runes Per 5 Sec."
+#ITEM_MOD_POWER_REGEN6_SHORT = "Runic Power Per 5 Sec."
+
+
+class Stat(object):
+	TABLE = {
+		1: "HEALTH",
+		3: "AGILITY",
+		4: "STRENGTH",
+		5: "INTELLECT",
+		6: "SPIRIT",
+		7: "STAMINA",
+		12: "DEFENSE_SKILL_RATING",
+		13: "DODGE_RATING",
+		14: "PARRY_RATING",
+		15: "BLOCK_RATING",
+		16: "HIT_MELEE_RATING",
+		17: "HIT_RANGED_RATING",
+		18: "HIT_SPELL_RATING",
+		19: "CRIT_MELEE_RATING",
+		20: "CRIT_RANGED_RATING",
+		21: "CRIT_SPELL_RATING",
+		22: "HIT_MELEE_TAKEN_RATING",
+		23: "HIT_RANGED_TAKEN_RATING",
+		24: "HIT_SPELL_TAKEN_RATING",
+		25: "CRIT_MELEE_TAKEN_RATING",
+		26: "CRIT_RANGED_TAKEN_RATING",
+		27: "CRIT_SPELL_TAKEN_RATING",
+		28: "HASTE_MELEE_RATING",
+		29: "HASTE_RANGED_RATING",
+		30: "HASTE_SPELL_RATING",
+		31: "HIT_RATING",
+		32: "CRIT_RATING",
+		33: "HIT_TAKEN_RATING",
+		34: "CRIT_TAKEN_RATING",
+		35: "RESILIENCE_RATING",
+		36: "HASTE_RATING",
+		37: "EXPERTISE_RATING",
+		38: "ATTACK_POWER",
+		39: "RANGED_ATTACK_POWER",
+		40: "FERAL_ATTACK_POWER",
+		41: "SPELL_HEALING_DONE",
+		42: "SPELL_DAMAGE_DONE",
+		43: "MANA_REGENERATION",
+		44: "ARMOR_PENETRATION_RATING",
+		45: "SPELL_POWER",
+		46: "HEALTH_REGENERATION",
+		47: "SPELL_PENETRATION",
+		48: "BLOCK_VALUE",
+		49: "MASTERY_RATING",
+		50: "EXTRA_ARMOR",
+		51: "FIRE_RESISTANCE",
+		52: "FROST_RESISTANCE",
+		54: "SHADOW_RESISTANCE",
+		55: "NATURE_RESISTANCE",
+		56: "ARCANE_RESISTANCE",
+	}
+	def __init__(self, id):
+		if id not in self.TABLE:
+			raise ValueError("Unknown stat: %r" % (id))
+		
+		self.id = id
+		
+		name = "ITEM_MOD_" + self.TABLE[id]
+		self.text = getattr(globalstrings, name)
+		self.name = getattr(globalstrings, name + "_SHORT")
+	
+	def getText(self, amount):
+		return self.text % (amount)
+	
+	def isSpecial(self):
+		return self.id > 10
