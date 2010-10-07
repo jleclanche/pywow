92214b8d80039ba2178d8b550ae10e324cbeb14b
Jerome Leclanche <jerome@leclan.ch>
2010-10-08 00:46:48 +0100

    game: Add DARKCYAN color

--- game/__init__.py
+++ game/__init__.py
@@ -5,16 +5,17 @@ Contains model logic for the game
 """
 
 # Colors
-BLUE   = 0x0080ff
-CYAN   = 0x66bbff
-GOLD   = 0xe5cc80
-GREEN  = 0x1eff00
-GREY   = 0x9d9d9d
-ORANGE = 0xff8000
-PURPLE = 0xb048f8
-RED    = 0xff2020
-YELLOW = 0xffd100
-WHITE  = 0xffffff
+BLUE     = 0x0080ff
+CYAN     = 0x66bbff
+DARKCYAN = 0x88aaff
+GOLD     = 0xe5cc80
+GREEN    = 0x1eff00
+GREY     = 0x9d9d9d
+ORANGE   = 0xff8000
+PURPLE   = 0xb048f8
+RED      = 0xff2020
+YELLOW   = 0xffd100
+WHITE    = 0xffffff
 
 class Model(object):
 	"""
