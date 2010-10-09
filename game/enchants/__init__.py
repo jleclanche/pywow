235475a4ecdbc83a3b7f35dced42b399005c74ea
Jerome Leclanche <jerome@leclan.ch>
2010-10-09 06:01:38 +0100

    game/enchants: Add enchants module

---
+++ game/enchants/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Enchants
+ - SpellItemEnchantment.dbc
+"""
+
+from .. import *
+
+
+class Enchant(Model):
+	pass
+
+class EnchantProxy(object):
+	"""
+	WDBC proxy for enchants
+	"""
+	def __init__(self, cls):
+		from pywow import wdbc
+		self.__file = wdbc.get("SpellItemEnchantment.dbc", build=-1)
+	
+	def get(self, id):
+		return self.__file[id]
+	
+	def getName(self, row):
+		return row.name_enus
