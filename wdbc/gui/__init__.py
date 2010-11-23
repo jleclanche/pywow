b696cda77cd0b6c7aad66c30e3ebc3270c80bab1
Jerome Leclanche <jerome@leclan.ch>
2010-11-23 02:30:36 +0000

    wdbc/gui: Cleanup & rewrite the code and use PySide instead of PyQt4

--- wdbc/gui/__init__.py
+++ wdbc/gui/__init__.py
@@ -3,136 +3,90 @@
 
 import operator
 import os
-import signal
-import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
 from binascii import hexlify
 from optparse import OptionParser
+from PySide.QtCore import *
+from PySide.QtGui import *
 from pywow import wdbc
 
-arguments = OptionParser()
-arguments.add_option("-b", "--build", type="int", dest="build", default=0)
-arguments.add_option("--get", action="store_true", dest="get", help="get from the environment")
 
-
-def main():
-	signal.signal(signal.SIGINT, signal.SIG_DFL)
-	app = QApplication(sys.argv)
-	args, name = arguments.parse_args(sys.argv[1:])
-	name = name[0]
-	if args.get:
-		file = wdbc.get(name, build=args.build)
-	else:
-		file = wdbc.fopen(name, build=args.build)
-	
-	w = MainWindow()
-	w.setFile(file, name)
-	w.show()
-	sys.exit(app.exec_())
-
+class WDBCClient(QApplication):
+	def __init__(self, argv):
+		QApplication.__init__(self, argv)
+		
+		arguments = OptionParser()
+		arguments.add_option("-b", "--build", type="int", dest="build", default=0)
+		arguments.add_option("--get", action="store_true", dest="get", help="get from the environment")
+		
+		args, files = arguments.parse_args(argv[1:])
+		name = files[0] # XXX
+		
+		if args.get:
+			file = wdbc.get(name, args.build)
+		else:
+			file = wdbc.fopen(name, args.build)
+		
+		self.mainWindow = MainWindow()
+		self.mainWindow.setFile(file)
 
 class MainWindow(QMainWindow):
-	def __init__(self, *pargs):
-		QMainWindow.__init__(self, *pargs)
+	def __init__(self, *args):
+		QMainWindow.__init__(self, *args)
 		self.resize(1332, 886)
+		self.setMinimumSize(640, 480)
 		
-		exit = QAction(QIcon("icons/exit.png"), "Exit", self)
-		exit.setShortcut("Ctrl+Q")
-		exit.setStatusTip("Exit application")
-		self.connect(exit, SIGNAL("triggered()"), SLOT("close()"))
+		fileMenu = self.menuBar().addMenu("&File")
+		fileMenu.addAction("Open", self, SLOT("open()"), "Ctrl+O")
+		fileMenu.addAction("Exit", self, SLOT("close()"), "Ctrl+Q")
 		
-		open = QAction(QIcon("icons/open.png"), "Open", self)
-		open.setShortcut("Ctrl+O")
-		open.setStatusTip("Open a new file")
-		openFileDialog = QFileDialog()
-		openFileDialog.connect(open, SIGNAL("triggered()"), SLOT("open()"))
+		centralWidget = QWidget(self)
+		self.setCentralWidget(centralWidget)
 		
-		menubar = self.menuBar()
-		mfile = menubar.addMenu("&File")
-		mfile.addAction(open)
-		mfile.addAction(exit)
-		
-		self.centralwidget = QWidget(self)
-		self.verticalLayout = QVBoxLayout(self.centralwidget)
-		
-		self.maintable = MainTable(self.centralwidget)
-		
-		self.verticalLayout.addWidget(self.maintable)
-		self.setCentralWidget(self.centralwidget)
-		
-		self.statusbar = QStatusBar()
-		self.setStatusBar(self.statusbar)
+		verticalLayout = QVBoxLayout(centralWidget)
+		self.maintable = MainTable(centralWidget)
+		verticalLayout.addWidget(self.maintable)
 	
-	def setFile(self, file, name):
-		self.setWindowTitle("%s - Sigrie Reader" % (name))
-		self.maintable.model.setFile(file)
+	def setFile(self, file):
+		self.setWindowTitle("%s - Sigrie Reader" % (file.file.name))
+		self.maintable.setFile(file)
 		msg = "%i rows - Using %s build %i" % (len(file), file.structure, file.build)
-		self.statusbar.showMessage(msg)
+		self.statusBar().showMessage(msg)
 
 
 class MainTable(QWidget):
-	def __init__(self, *pargs):
-		QWidget.__init__(self, *pargs)
+	def __init__(self, *args):
+		QWidget.__init__(self, *args)
 		
 		# create table
-		#self.get_table_data()
-		table = self.createTable()
+		self.table = table = QTableView()
+		table.setModel(TableModel(self))
+		table.verticalHeader().setVisible(True)
+		table.setSortingEnabled(True)
 		
 		# layout
 		layout = QVBoxLayout()
 		layout.addWidget(table)
 		self.setLayout(layout)
 	
-	def createTable(self):
-		# create the view
-		tv = QTableView()
-		
-		# set the table model
-		self.model = MainTableModel(self)
-		tv.setModel(self.model)
-		
-		# set the minimum size
-		tv.setMinimumSize(400, 300)
-		
-		# hide vertical header
-		vh = tv.verticalHeader()
-		vh.setVisible(True)
-		
-		# set horizontal header properties
-		hh = tv.horizontalHeader()
-#		hh.setStretchLastSection(True)
-		
-		# enable sorting
-		tv.setSortingEnabled(True)
-
-		return tv
+	def setFile(self, file):
+		return self.table.model().setFile(file)
 
 
-class MainTableModel(QAbstractTableModel):
-	def __init__(self, *pargs):
-		super(MainTableModel, self).__init__(*pargs)
+class TableModel(QAbstractTableModel):
+	def __init__(self, *args):
+		QAbstractTableModel.__init__(self, *args)
 		self.table_data = []
 		self.header_data = []
-	
-	def setFile(self, file):
-		self.emit(SIGNAL("layoutAboutToBeChanged()"))
-		self.table_data = file.rows()
-		self.header_data = file.structure.column_names
-		self.structure = file.structure
-		self.emit(SIGNAL("layoutChanged()"))
-	
-	def rowCount(self, parent):
-		return len(self.table_data)
 
 	def columnCount(self, parent):
 		return len(self.header_data)
 	
+	def rowCount(self, parent):
+		return len(self.table_data)
+	
 	def data(self, index, role):
-		if not index.isValid():
-			return QVariant()
-		elif role != Qt.DisplayRole:
-			return QVariant()
+		if not index.isValid() or role != Qt.DisplayRole:
+			return
 		
 		cell = self.table_data[index.row()][index.column()]
 		field = self.structure[index.column()]
@@ -140,20 +94,36 @@ class MainTableModel(QAbstractTableModel):
 			cell = hexlify(cell)
 		if isinstance(cell, str) and len(cell) > 200:
 			cell = cell[:200] + "..."
-		return QVariant(cell)
+		return cell
 	
 	def headerData(self, col, orientation, role):
 		if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-			return QVariant(self.header_data[col])
+			return self.header_data[col]
 		return QAbstractTableModel.headerData(self, col, orientation, role)
-
-	def sort(self, Ncol, order):
+	
+	def setFile(self, file):
 		self.emit(SIGNAL("layoutAboutToBeChanged()"))
-		self.table_data = sorted(self.table_data, key=operator.itemgetter(Ncol))
+		self.table_data = file.rows()
+		self.header_data = file.structure.column_names
+		self.structure = file.structure
+		self.emit(SIGNAL("layoutChanged()"))
+
+	def sort(self, column, order):
+		self.emit(SIGNAL("layoutAboutToBeChanged()"))
+		self.table_data = sorted(self.table_data, key=operator.itemgetter(column))
 		if order == Qt.AscendingOrder:
 			self.table_data.reverse()
 		self.emit(SIGNAL("layoutChanged()"))
 
+
+def main():
+	import signal
+	import sys
+	signal.signal(signal.SIGINT, signal.SIG_DFL)
+	app = WDBCClient(sys.argv)
+	
+	app.mainWindow.show()
+	sys.exit(app.exec_())
 
 if __name__ == "__main__":
 	main()
