50e69ab22e55a8086c60adbf9943bcf74d1d42ae
Jerome Leclanche <jerome@leclan.ch>
2011-08-30 05:43:27 +0100

    wdbc/utils: in fopen(), only open the argument as a file if it is a string

--- wdbc/utils.py
+++ wdbc/utils.py
@@ -18,22 +18,24 @@ def generate_structure(db):
 	TODO improve it, guess floats and shorter fields.
 	"""
 	from .structures import GeneratedStructure
-	
+
 	if db.header.field_count * 4 == db.header.reclen:
 		structure_string = "i" * db.header.field_count
 	else:
 		raise NotImplementedError
-	
+
 	return GeneratedStructure(structure_string)
 
 
-def fopen(name, build=0, structure=None, environment={}):
+def fopen(f, build=0, structure=None, environment={}):
 	from .structures import StructureNotFound, getstructure
-	
-	file = open(name, "rb")
-	filename = getfilename(name)
-	signature = file.read(4)
-	
+
+	if isinstance(f, basestring):
+		# open() the file only if passing a path
+		f = open(f, "rb")
+	filename = getfilename(f.name)
+	signature = f.read(4)
+
 	if signature == "WDB2" or signature == "WCH2":
 		from .db2 import DB2File
 		cls = DB2File
@@ -41,7 +43,7 @@ def fopen(name, build=0, structure=None, environment={}):
 			_structure = structure or getstructure(filename)
 		except StructureNotFound:
 			pass
-	
+
 	elif signature == "WDBC":
 		from .dbc import DBCFile, InferredDBCFile
 		cls = DBCFile
@@ -53,36 +55,36 @@ def fopen(name, build=0, structure=None, environment={}):
 			cls = DBCFile
 			if getattr(_structure, "implicit_id", None):
 				cls = InferredDBCFile
-	
+
 	elif not signature:
 		raise IOError()
-	
+
 	elif name.endswith(".wcf"):
 		from .dbc import WCFFile
 		cls = WCFFile
 		structure = structure or getstructure(filename)
-	
+
 	else:
 		from .wdb import WDBFile
 		cls = WDBFile
-	
-	file = cls(file, build=build, structure=structure, environment=environment)
-	file.preload()
-	
-	return file
+
+	f = cls(f, build=build, structure=structure, environment=environment)
+	f.preload()
+
+	return f
 
 
 def new(name, build=0, structure=None, environment={}):
 	from .structures import getstructure
 	file = open(name, "wb")
-	
+
 	if not structure:
 		structure = getstructure(getfilename(name), build=build)
-	
+
 	if structure.signature == "WDBC":
 		from .dbc import DBCFile
 		return DBCFile(file, build=build, structure=structure, environment=environment)
-	
+
 	from .wdb import WDBFile
 	return WDBFile(file, build=build, structure=structure, environment=environment)
 
@@ -92,9 +94,9 @@ def get(name, build):
 	if build == -1:
 		from .environment import get_latest_build
 		build = get_latest_build()
-	
+
 	if build not in __envcache:
 		from .environment import Environment
 		__envcache[build] = Environment(build)
-	
+
 	return __envcache[build][name]
