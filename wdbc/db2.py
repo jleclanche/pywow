119124d1ecc924fc3a4f6ec8d0a8c447b7655492
Jerome Leclanche <jerome@leclan.ch>
2012-01-03 10:26:13 +0000

    wdbc/db2: Cleanup DB2Files. Get rid of the old DB2Header, properly reuse DBCFile methods, etc

--- wdbc/db2.py
+++ wdbc/db2.py
@@ -1,87 +1,49 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
 from struct import unpack
 from .dbc import DBCFile
 from .log import log
 from .structures import getstructure, LocalizedStringField, LocalizedField
 from .utils import getfilename
 
+
 SEEK_CUR = 1 # os.SEEK_CUR
-
-class DB2Header(object):
-	def __len__(self):
-		if self.build <= 12880:
-			return 32
-		return 48
-
-	def load(self, file):
-		file.seek(0)
-		self.signature, self.row_count, self.field_count, self.reclen, self.stringblocksize, self.dbhash, self.build, self.timestamp = unpack("<4s7i", file.read(32))
-		if self.build > 12880:
-			if self.signature == "WCH2" and self.build < 12942:
-				# Work around a bug in cataclysm beta which doesn't take in account the first \0
-				log.warning("Old adb file, working around stringblock bug")
-				self.stringblocksize += 1
-			self.lookup_start, self.lookup_end, self.locale, self.unk3 = unpack("<4i", file.read(16))
-
 
 class DB2File(DBCFile):
 	"""
 	New DB format introduced in build 12803
 	"""
 
-	def __init__(self, file, build, structure, environment):
-		super(DBCFile, self).__init__(file, build, structure, environment)
-		self.header = DB2Header()
-		self.header.load(file)
-		self.build = build or self.header.build
-		self.__load_structure(structure)
+	def _readHeader(self):
+		data = self.file.read(32)
+		self.headerStructure = "<4s7i"
+		fields = ["signature", "row_count", "field_count", "reclen", "stringblocksize", "dbhash", "build", "timestamp"]
+		signature, row_count, field_count, reclen, stringblocksize, dbhash, build, timestamp = unpack(self.headerStructure, data)
 
-	def __load_structure(self, structure):
-		name = getfilename(self.file.name)
-		try:
-			self.structure = getstructure(name, self.build, parent=self)
-		except StructureNotFound:
-			self.structure = generate_structure(self)
 
-		# Generate the Localized Fields
-		fieldidx = []
-		for i, field in enumerate(self.structure):
-			if isinstance(field, LocalizedField):
-				fieldidx.append((i, field.name))
+		if build <= 12880:
+			# Old style headers, 32 bytes
+			DB2Header = namedtuple("DB2Header", fields)
+			header = DB2Header(signature, row_count, field_count, reclen, stringblocksize, dbhash, build, timestamp)
+		else:
+			self.headerStructure = "<4s11i"
+			fields += ["lookup_start", "lookup_end", "locale", "unk"]
+			DB2Header = namedtuple("DB2Header", fields)
+			lookup_start, lookup_end, locale, unk = unpack("<4i", self.file.read(4*4))
 
-		if fieldidx:
-			from copy import copy
-			fields = LocalizedStringField(build=self.build)
-			for i, name in reversed(fieldidx):
-				# Build a copy of the fields
-				toinsert = [copy(field).rename("%s_%s" % (name, field.name)) for field in fields]
-				self.structure[i:i+1] = toinsert
+			if signature == "WCH2" and build < 12942:
+				# Work around a bug in cataclysm beta which doesn't take in account the first \0
+				log.warning("Old adb file, working around stringblock bug")
+				stringblocksize += 1
+			header = DB2Header(signature, row_count, field_count, reclen, stringblocksize, dbhash, build, timestamp, lookup_start, lookup_end, locale, unk)
 
-		log.info("Using %s build %i" % (self.structure, self.build))
-		self.check_integrity()
-
-	def preload(self):
-		f = self.file
-		f.seek(len(self.header))
-
-		if self.build > 12880:
-			if self.header.lookup_start != self.header.lookup_end:
-				size = (self.header.lookup_end - self.header.lookup_start + 1) * 6
+			# Skip the index block... we'll have to use it some day..
+			if lookup_start != lookup_end:
+				size = (lookup_end - lookup_start + 1) * 6
 				if size < 0:
 					log.error("lookup size < 0: %i. This file is corrupt. Expect breakage!" % (size))
 				else:
-					lookups = f.read(size)
+					self.file.seek(size, SEEK_CUR)
 
-		rows = 0
-		field = self.structure[0]
-		row_header_size = field.size
-		reclen = self.header.reclen
-		while rows < self.header.row_count:
-			address = f.tell() # Get the address of the full row
-			id = self._parse_field(f, field)
-
-			self._add_row(id, address, reclen)
-
-			f.seek(reclen - row_header_size, SEEK_CUR) # minus length of id
-			rows += 1
+		return header
