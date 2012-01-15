ae16427d171f9a6bbf4964de1d6e57508392fdb6
Jerome Leclanche <jerome@leclan.ch>
2012-01-15 19:34:45 +0000

    environment: Create and use a Base class rather than a bunch of messy functions and methods

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,61 +1,87 @@
-# -*- coding: utf-8 -*-
-
+"""
+WoW-style MPQ-based environments
+"""
 import os
 import re
 import mpq
 
 
-def basesToBuild(base):
-	sre = re.compile(r"^(\d+).direct$")
-	ret = {}
-	for f in os.listdir(base):
-		# Here we parse each <build>.direct/
-		match = sre.match(os.path.basename(f))
-		if match:
-			ret[int(match.groups()[0])] = os.path.join(base, f)
-
-	return ret
-
-def defaultBase():
-	# Try $MPQ_BASE_DIR, otherwise use ~/mpq/WoW
-	return os.environ.get("MPQ_BASE_DIR", os.path.join(os.path.expanduser("~"), "mpq", "WoW"))
-
-def highestBase():
-	bases = basesToBuild(defaultBase())
-	return os.path.join(bases[sorted(bases.keys())[-1]], "Data")
-
-def readBuild(base):
-	"""
-	Returns the build part of a base (eg 12699 from '12699.direct')
-	"""
-	return int(re.match(r"^(\d+).direct$", os.path.basename(base)).groups()[0])
-
-
 class BuildNotFound(Exception):
 	pass
 
-class Environment(object):
-	def __init__(self, build, locale="enUS", base=defaultBase()):
-		base = self.baseForBuild(base, build)
-		self.base = os.path.join(base, "Data")
-		self.build = build
-		self.locale = locale
-		self.path = os.path.join(self.base, locale, "locale-%s.MPQ" % (locale))
-
-		self.mpq = mpq.MPQFile(self.path)
-		if build != readBuild(base):
-			for patch in self.patchList():
-				self.mpq.patch(patch)
-
-		self._cache = {}
+class Base(object):
+	def __init__(self, rawPath, build=None):
+		self.rawPath = rawPath
+		if build is not None:
+			self.setBuild(build)
 
 	def __repr__(self):
-		return "Environment(build=%r, locale=%r, base=%r)" % (self.build, self.locale, self.base)
+		if hasattr(self, "_path"):
+			return "Base(%r)" % (self.path())
+		return "Base(%r)" % (self.rawPath)
 
-	@classmethod
-	def baseForBuild(cls, base, build):
+	def build(self):
+		return int(re.match(r"^(\d+).direct$", os.path.basename(self.path())).groups()[0])
+
+	def builds(self):
+		"""
+		Returns a dict of base paths for build numbers in {build: path} format
+		"""
+		sre = re.compile(r"^(\d+).direct$")
+		ret = {}
+		for f in os.listdir(self.rawPath):
+			# Here we parse each <build>.direct/
+			match = sre.match(os.path.basename(f))
+			if match:
+				ret[int(match.groups()[0])] = f
+
+		return ret
+
+	def dataDir(self):
+		return os.path.join(self.path(), "Data")
+
+	def localeDir(self, locale):
+		return os.path.join(self.dataDir(), locale)
+
+	def path(self):
+		if not hasattr(self, "_path"):
+			raise RuntimeError("Cannot access Base.path() if Base does not have a build")
+
+		return os.path.join(self.rawPath, self._path)
+
+	def patchFiles(self, locale="enUS"):
+		"""
+		Returns a dict of build: patch MPQs.
+		"""
+		files = {}
+
+		# Old-style wow-updates (oldest) first
+		path = self.dataDir()
+		sre = re.compile(r"^wow-update-(\d+).MPQ$")
+		for f in os.listdir(path):
+			match = sre.match(os.path.basename(f))
+			if match:
+				fileBuild = int(match.groups()[0])
+				files[fileBuild] = [os.path.join(path, f)]
+
+		# Special cases:
+		# wow-update*-13623 has both old-style and new-style patches.
+		# The new style ones are corrupt. We'll just assume that if
+		# we have both old-style and new-style, old-style takes priority.
+		path = self.localeDir(locale)
+		sre = re.compile(r"^wow-update-%s-(\d+).MPQ$" % (locale))
+		for f in os.listdir(path):
+			match = sre.match(os.path.basename(f))
+			if match:
+				fileBuild = int(match.groups()[0])
+				if fileBuild not in files:
+					files[fileBuild] = [os.path.join(path, f)]
+
+		return files
+
+	def setBuild(self, build):
 		highestMatch = 0
-		bases = basesToBuild(base)
+		bases = self.builds()
 		for baseBuild in bases:
 			# We want the highest possible match:
 			# - filter out anything higher than the requested build
@@ -66,42 +92,40 @@ class Environment(object):
 		if not highestMatch:
 			raise BuildNotFound(build)
 
-		return bases[highestMatch]
+		self._path = bases[highestMatch]
 
-	@classmethod
-	def patchFiles(cls, base, locale="enUS"):
-		"""
-		Returns a dict of build: patch MPQs.
-		base argument must be a Data/-base
-		"""
-		files = {}
 
-		# Old-style wow-updates (oldest) first
-		sre = re.compile(r"^wow-update-(\d+).MPQ$")
-		for f in os.listdir(base):
-			match = sre.match(os.path.basename(f))
-			if match:
-				fileBuild = int(match.groups()[0])
-				files[fileBuild] = [os.path.join(base, f)]
+def defaultBase():
+	# Try $MPQ_BASE_DIR, otherwise use ~/mpq/WoW
+	return Base(os.environ.get("MPQ_BASE_DIR", os.path.join(os.path.expanduser("~"), "mpq", "WoW")))
 
-		# Special cases:
-		# wow-update*-13623 has both old-style and new-style patches.
-		# The new style ones are corrupt. We'll just assume that if
-		# we have both old-style and new-style, old-style takes priority.
-		sre = re.compile(r"^wow-update-%s-(\d+).MPQ$" % (locale))
-		base = os.path.join(base, locale)
-		for f in os.listdir(base):
-			match = sre.match(os.path.basename(f))
-			if match:
-				fileBuild = int(match.groups()[0])
-				if fileBuild not in files:
-					files[fileBuild] = [os.path.join(base, f)]
+def highestBase():
+	base = defaultBase()
+	bases = base.builds()
+	base.setBuild(sorted(bases.keys())[-1])
+	return base
 
-		return files
 
-	@classmethod
-	def highestBuild(cls):
-		return sorted(cls.patchFiles(highestBase()).keys())[-1]
+class Environment(object):
+	def __init__(self, build, locale="enUS", base=defaultBase()):
+		base.setBuild(build)
+		self.base = base
+		self.build = build
+		self.locale = locale
+		self.path = os.path.join(self.base.localeDir(locale), "locale-%s.MPQ" % (locale))
+
+		self.mpq = mpq.MPQFile(self.path)
+		if build != base.build():
+			for patch in self.patchList():
+				self.mpq.patch(patch)
+
+		self._cache = {}
+
+	def __repr__(self):
+		return "Environment(build=%r, locale=%r, base=%r)" % (self.build, self.locale, self.base)
+
+	def highestBuild():
+		return sorted(highestBase().patchFiles().keys())[-1]
 
 	def _dbFileName(self, name):
 		# In order to avoid duplicates, we need to standardize the filename
@@ -155,7 +179,7 @@ class Environment(object):
 		return self.mpq.open(file)
 
 	def patchList(self):
-		patches = self.patchFiles(base=self.base, locale=self.locale)
+		patches = self.base.patchFiles()
 		builds = sorted(patches.keys())
 		ret = []
 		for build in builds:
